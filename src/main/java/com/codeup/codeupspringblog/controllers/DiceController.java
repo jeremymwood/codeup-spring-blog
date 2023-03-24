d4ee36435ef97c903ece3c75cdb592d1980df067
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-24 16:35:51 -0500

    1635

--- src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
@@ -7,15 +7,14 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class DiceController {
     @GetMapping("/roll-dice")
-    public String getDice() {
-        return "roll-dice";
-    }
+    public String getDice() {return "roll-dice";}
 
     @GetMapping("/roll-dice/{n}")
-    public String rollDice(@RequestParam(name = "n") int n, Model model) {
-        model.addAttribute("n", n);
+    public String rollDice(@PathVariable int n, Model model) {
         int diceRoll = (int) (Math.random() * 6) +1;
-        return "diceRoll/" + n;
+        model.addAttribute("n", "Your guess was: " + n + ". \n The answer was: " + diceRoll);
+//        return "roll-dice/" + n;
+        return "roll-dice";
     }
 
 }
