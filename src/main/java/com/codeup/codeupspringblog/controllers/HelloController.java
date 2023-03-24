047def897c1d7006c79c265069aa1a0f4707f9b4
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-24 12:27:33 -0500

    1227

--- src/main/java/com/codeup/codeupspringblog/controllers/HelloController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HelloController.java
@@ -1,10 +1,14 @@
 package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //tell spring this class is a controller
 @Controller
+@RequestMapping(path = "/join")
 class HelloController {
 
 //    method for url
@@ -21,12 +25,29 @@ class HelloController {
 //        return "Hello " + name + "!";
 //    }
 
-@RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-@ResponseBody
-public String addOne(@PathVariable int number) {
-    return number + " + 1 = " + (number + 1) + "!";
+//@RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+//@ResponseBody
+//public String addOne(@PathVariable int number) {
+//    return number + " + 1 = " + (number + 1) + "!";
+//}
+//
+//    @GetMapping("/hello/{name}")
+//    public String sayHello(@PathVariable String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "hello";
+//    }
+
+    @GetMapping()
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping()
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
 }
 
 
-
-}
--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage() {
-        return "This is the landing page!";
+//    @GetMapping("/home/")
+    @GetMapping("/home/{personName}")
+//    @ResponseBody
+    public String welcome(@RequestParam(name = "age") int age, @PathVariable String personName, Model model) {
+//        for /home
+        model.addAttribute("name", "Jeremy Wood");
+        model.addAttribute("personName", personName);
+        model.addAttribute("personAge", age);
+        return "home/home";
     }
+
 }
