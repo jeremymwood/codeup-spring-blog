4dc37fdbd398bded4c74b756a5c8abeb5da0a718
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-25 15:15:15 -0500

    1513-bootstrap connected

--- src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/DiceController.java
@@ -11,9 +11,15 @@ public class DiceController {
 
     @GetMapping("/roll-dice/{n}")
     public String rollDice(@PathVariable int n, Model model) {
-        int diceRoll = (int) (Math.random() * 6) +1;
-        model.addAttribute("n", "Your guess was: " + n + ". \n The answer was: " + diceRoll);
-//        return "roll-dice/" + n;
+        int answer = (int) (Math.random() * 6) +1;
+        model.addAttribute("n", n);
+        model.addAttribute("answer", answer);
+
+//        if (n == answer) {
+//            return "Huzzah! You guessed " + n + ", and the answer was " + answer + ".";
+//        } else {
+//            return "Boo! You guessed " + n + ", and the answer was " + answer + ".";
+//        }
         return "roll-dice";
     }
 
